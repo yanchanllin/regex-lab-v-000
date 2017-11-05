b5ca43f05e15122efbd413b150ca1191f96f1a1d
YanC Lin <yonalin1@gmail.com>
2017-11-05 05:19:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-word.scan(/[aeiou]/) 
+word.scan(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.
+# phone.
 end
