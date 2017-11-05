656598fc05cae64674f1e0200784865d8b7cde34
YanC Lin <yonalin1@gmail.com>
2017-11-05 05:26:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-word.scan(/[aeiou]/)
+word.match(/^[aeiouAEIOU]+\w/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un...ing/)
+text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-text.grep(/\w{5}/)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-# text.scan(/^\capitalized
+text.match(/^[A-Z].+[\.!?]$/)
 end
 
 def valid_phone_number?(phone)
-# phone.
+phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
 end
