963af0d0aa73b3e8c0bc35aa9e59a19c109863a9
YanC Lin <yonalin1@gmail.com>
2017-11-05 05:29:30 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
-word.scan(/[aeiou]/)
+ if word.match(/^[aeiouAEIOU]+\w/)
+   then
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un...ing/)
+text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-text.grep(/\w{5}/)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan(/^\capitalized
+  if text.match(/^[A-Z].+[\.!?]$/)
+  then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-phone.
+   if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    then
+      return true
+    else
+      return false
+    end
 end
