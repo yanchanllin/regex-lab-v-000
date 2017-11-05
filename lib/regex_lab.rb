1e2f49406a81f7441af39a8a27a784d924f34bed
YanC Lin <yonalin1@gmail.com>
2017-11-05 05:18:29 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+word.scan(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un...ing/)
 end
 
 def words_five_letters_long(text)
-
+text.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan(/^\capitalized
 end
 
 def valid_phone_number?(phone)
-
+phone.
 end
