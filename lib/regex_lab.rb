b1b47254a12c73fb3265f0a3e34e1be336a444b7
YanC Lin <yonalin1@gmail.com>
2017-11-05 05:28:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-word.match(/^[aeiouAEIOU]+\w/)
+if word.match(/^[aeiouAEIOU]+\w/)
+then 
+    return true
+  else 
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +15,17 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].+[\.!?]$/)
+if text.match(/^[A-Z].+[\.!?]$/)
+then 
+    return true
+  else 
+    return false
 end
 
 def valid_phone_number?(phone)
-phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  then 
+      return true
+    else 
+      return false
 end
